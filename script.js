44224266aa7a1d84d0750ab3c5ab29c98adc0107
Olivier Gauthier <o.b.gauthier@gmail.com>
2022-04-13 20:02:44 -0400

    Fix the console logging

--- script.js
+++ script.js
@@ -9,26 +9,25 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
   
 if (playerSelection === "rock" && computerSelection === "rock") {
-    console.log("You tie")
+    return "You tie"
 } else if (playerSelection === "rock" && computerSelection === "paper") {
-    console.log("You lose! Paper beats Rock")
+    return "You lose! Paper beats Rock"
 } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    console.log("You win! Rock beats Scissors")
+    return "You win! Rock beats Scissors"
 } else if (playerSelection === "paper" && computerSelection === "paper") {
-    console.log("You tie")
+    return "You tie"
 } else if (playerSelection === "paper" && computerSelection === "rock") {
-    console.log("You win! Paper beat Rock")
+    return "You win! Paper beat Rock"
 } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    console.log("You lose! Scissors beat Paper")
+    return "You lose! Scissors beat Paper"
 } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    console.log("You win! Scissors beat Paper")
+    return "You win! Scissors beat Paper"
 } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    console.log("You lose! Rock beat Scissors")
+    return "You lose! Rock beat Scissors"
 } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-    console.log("You tie")
+    return "You tie"
 }
 }
 
 let playerSelection = window.prompt("Rock, paper or scissors?").toLowerCase()
-console.log(playerSelection)
-playRound(playerSelection, computerPlay())
+console.log(playRound(playerSelection, computerPlay()))
